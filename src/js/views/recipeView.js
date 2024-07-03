484653b01ea80f35cb8825352b341106459dcacf
ardsen <as.ardasen@gmail.com>
2024-07-03 18:43:28 +0300

    fraction api bug fix

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,7 +1,7 @@
 import View from './View.js';
 
 import icons from 'url:../../img/icons.svg';
-import Fraction from 'fraction.js';
+import Fraction from './fraction.js';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
